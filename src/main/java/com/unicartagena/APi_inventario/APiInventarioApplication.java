e0c29d1ade33d0aad60407d83119f038602d8641
DarkSevenX <nargumedor@unicartagena.edu.co>
2026-03-05 21:51:55 -0500

    comments

--- src/main/java/com/unicartagena/APi_inventario/APiInventarioApplication.java
+++ src/main/java/com/unicartagena/APi_inventario/APiInventarioApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class APiInventarioApplication {
-
+	// hola nosequeponerjkl
 	public static void main(String[] args) {
 		SpringApplication.run(APiInventarioApplication.class, args);
 	}
